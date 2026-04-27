920e2db10eec95532f8eda8b88fd070c563c7096
Tonmoy chakrabarty joy <tonmoy.chakrabarty.joy01@gmail.com>
2026-04-27 19:43:57 +0600

    Add student result show form

---
+++ OOP2_final_project/Form14.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace OOP2_final_project
+{
+    partial class Form14
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            lb_co_Name = new Label();
+            lbl_result = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(54, 108);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(924, 414);
+            dataGridView1.TabIndex = 0;
+            // 
+            // lb_co_Name
+            // 
+            lb_co_Name.AutoSize = true;
+            lb_co_Name.BackColor = SystemColors.ScrollBar;
+            lb_co_Name.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lb_co_Name.ForeColor = Color.Black;
+            lb_co_Name.Location = new Point(398, 9);
+            lb_co_Name.Name = "lb_co_Name";
+            lb_co_Name.Size = new Size(194, 35);
+            lb_co_Name.TabIndex = 14;
+            lb_co_Name.Text = "TCJ Academy";
+            // 
+            // lbl_result
+            // 
+            lbl_result.AutoSize = true;
+            lbl_result.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_result.Location = new Point(54, 62);
+            lbl_result.Name = "lbl_result";
+            lbl_result.Size = new Size(90, 32);
+            lbl_result.TabIndex = 15;
+            lbl_result.Text = "Result";
+            // 
+            // Form14
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ScrollBar;
+            ClientSize = new Size(1006, 534);
+            Controls.Add(lbl_result);
+            Controls.Add(lb_co_Name);
+            Controls.Add(dataGridView1);
+            Name = "Form14";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Show result";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label lb_co_Name;
+        private Label lbl_result;
+    }
+}
