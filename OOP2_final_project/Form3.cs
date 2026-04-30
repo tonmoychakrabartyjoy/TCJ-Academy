a66cacec7d719545bb17c49974afd00366090197
Tonmoy chakrabarty joy <tonmoy.chakrabarty.joy01@gmail.com>
2026-04-30 18:33:06 +0600

    Ass Exam button click then go to add exam page

--- OOP2_final_project/Form3.cs
+++ OOP2_final_project/Form3.cs
@@ -15,6 +15,19 @@ namespace OOP2_final_project
             InitializeComponent();
         }
 
+
+        private void btn_add_exam_Click(object sender, EventArgs e)
+        {
+            Form4 create_exam = new Form4();
+            create_exam.Show();
+
+            this.Hide();
+        }
+
+
+
+
+
         private void Form3_Load(object sender, EventArgs e)
         {
 
@@ -29,5 +42,7 @@ namespace OOP2_final_project
         {
 
         }
+
+        
     }
 }
