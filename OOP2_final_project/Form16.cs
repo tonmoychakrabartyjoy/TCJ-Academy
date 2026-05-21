3028e0445b842fee2792eb998327fa2e913d65d0
Tonmoy chakrabarty joy <tonmoy.chakrabarty.joy01@gmail.com>
2026-05-21 17:15:51 +0600

    fix

--- OOP2_final_project/Form16.cs
+++ OOP2_final_project/Form16.cs
@@ -22,7 +22,7 @@ namespace OOP2_final_project
         }
 
 
-        // Extra part
+        // Auto generated code part starts here
 
         private void txt_salary_KeyDown(object sender, KeyEventArgs e)
         {
@@ -40,8 +40,8 @@ namespace OOP2_final_project
         }
 
 
-        // End of extra part
-
+       
+        // Auto generated code part ends here
 
         private void lb_co_Name_Click(object sender, EventArgs e)
         {
